339fa9b1322dab889587416d75d040481f476a72
EnixWord <116887494+EnixWord@users.noreply.github.com>
2023-02-24 16:59:45 +0300

    Add add and remove functions to my To-Do-List

--- src/index.js
+++ src/index.js
@@ -1,33 +1,74 @@
 import './style.css';
 
-const tasksData = [
-  {
-    description: 'task 1',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'task 2',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'task 3',
-    completed: false,
-    index: 0,
-  },
-];
+class TodoData {
+  constructor() {
+    if (window.localStorage.getItem('tasksData') !== undefined) {
+      this.tasksData = JSON.parse(window.localStorage.getItem('tasksData'));
+    } else {
+      this.tasksData = [];
+    }
+  }
 
+  addTaskEntry(task) {
+    this.tasksData.push(task);
+    this.updateLocalStorage();
+  }
+
+  deleteTaskEntry(index) {
+    this.tasksData = this.tasksData.filter((task) => task.index !== index);
+    this.updateLocalStorage();
+  }
+
+  updateLocalStorage() {
+    localStorage.setItem('tasksData', JSON.stringify(this.tasksData));
+    this.tasksData = JSON.parse(window.localStorage.getItem('tasksData'));
+  }
+}
+
+const todoData = new TodoData();
 const todoListEl = document.getElementById('todo-list');
+const formEl = document.getElementById('add-todo-form');
+const inputEl = document.getElementById('add-task');
+const deleteBtnEls = document.getElementsByClassName('delete-btn');
 
 const renderTasks = () => {
-  tasksData.forEach((task, forLoopIndex) => {
+  todoListEl.innerHTML = '';
+  todoData.tasksData.forEach((task, forLoopIndex) => {
     const listEl = document.createElement('li');
     listEl.classList.add('list-item');
-    listEl.innerText = task.description;
-    task.index = forLoopIndex;
+    const listDescriptionEl = document.createElement('span');
+    listDescriptionEl.classList.add('task-entry');
+    listDescriptionEl.innerText = task.description;
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete-btn');
+    task.index = forLoopIndex + 1;
+    listEl.append(listDescriptionEl, deleteBtn);
     todoListEl.appendChild(listEl);
+  });
+
+  const deleteBtns = Array.from(deleteBtnEls);
+  deleteBtns.forEach((btn, i) => {
+    btn.addEventListener('click', () => {
+      const index = i + 1;
+      todoData.deleteTaskEntry(index);
+      todoData.tasksData.forEach((task, i) => {
+        task.index = i + 1;
+      });
+      todoData.updateLocalStorage();
+      renderTasks();
+    });
   });
 };
 
-renderTasks(tasksData);
+renderTasks();
+
+formEl.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const task = {
+    description: inputEl.value,
+    completed: false,
+    index: todoData.tasksData.length,
+  };
+  todoData.addTaskEntry(task);
+  renderTasks();
+});
